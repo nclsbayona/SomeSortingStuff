43c4388f46f549fa1e1a5af36947ce401c881076
nclsbayona <nbayonaord@gmail.com>
2020-12-10 11:48:23 -0500

    Java

--- JavaTrial.java
+++ JavaTrial.java
@@ -1,7 +1,9 @@
 public class JavaTrial {
     public static void main(String[] args) {
-        Integer[] array={89, 144, 55, 1, 13, 21, 2, 34, 0, 1, 8, 5, 3};
-        PatienceSort<Integer> patienceSort=new PatienceSort<>(array);
+        Integer[] array = { 89, 144, 55, 1, 13, 21, 2, 34, 0, 1, 8, 5, 3 };
+        PatienceSort<Integer> patienceSort = new PatienceSort<>(array);
+        patienceSort.patienceSort();
+        patienceSort.addElement(233);
         patienceSort.patienceSort();
     }
 }
--- PatienceSort.java
+++ PatienceSort.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 public class PatienceSort<T extends Comparable<T>> {
     private ArrayList<T> vector;
 
@@ -9,25 +8,53 @@ public class PatienceSort<T extends Comparable<T>> {
             this.vector.add(element);
     }
 
-    public void patienceSort(){
-        ArrayList<ArrayList<T>> vecStacks=new ArrayList<>();
-        for (T element:this.vector){
-            boolean inserted=false;
-            for (int i=0;i<vecStacks.size()&&!inserted;++i){
-                ArrayList<T> stack=vecStacks.get(i);
-                if (stack.get(stack.size()-1).compareTo(element)>=0){
-                    inserted=true;
+    public ArrayList<T> getVector() {
+        return this.vector;
+    }
+
+    public void addElement(T element) {
+        this.vector.add(element);
+    }
+
+    public void patienceSort() {
+        ArrayList<ArrayList<T>> vecStacks = new ArrayList<>();
+        int size = this.vector.size();
+        for (T element : this.vector) {
+            boolean inserted = false;
+            for (int i = 0; i < vecStacks.size() && !inserted; ++i) {
+                ArrayList<T> stack = vecStacks.get(i);
+                if (stack.get(stack.size() - 1).compareTo(element) >= 0) {
+                    inserted = true;
                     stack.add(element);
                 }
             }
-            if (!inserted){
-                ArrayList<T> stack=new ArrayList<>();
+            if (!inserted) {
+                ArrayList<T> stack = new ArrayList<>();
                 stack.add(element);
                 vecStacks.add(stack);
             }
         }
-        for (ArrayList<T> arrayList:vecStacks){
-
-        }
+        this.vector.clear();
+        for (int i = 0; i < size; ++i)
+            getLowest(vecStacks);
+        System.out.println(this.vector);
     }
+
+    private void getLowest(ArrayList<ArrayList<T>> vecStacks) {
+        T lowest = null;
+        int index = -1;
+        for (int i = 0; i < vecStacks.size() && !vecStacks.isEmpty(); ++i) {
+            ArrayList<T> stack = vecStacks.get(i);
+            if (lowest == null || lowest.compareTo(stack.get(stack.size() - 1)) > 0) {
+                index = i;
+                lowest = stack.get(stack.size() - 1);
+            }
+        }
+        if (index != -1) {
+            vecStacks.get(index).remove(lowest);
+            if (vecStacks.get(index).isEmpty())
+                vecStacks.remove(index);
+            this.vector.add(lowest);
+        }
+    };
 }
